f6e2fce528fdbb9b5146ba1635873158fa758a40
unknown <kkaturi2003@gmail.com>
2026-03-14 00:19:14 +0530

    food menu display is added

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/foodDisplay/foodDisplay'
 const Home = () => {
     const[category,setCategory] = useState("All");
     const[view, setView] = useState(false);
@@ -9,6 +10,7 @@ const Home = () => {
     <div>
         <Header view = {view} setView = {setView}/>
         <ExploreMenu category ={category} setCategory ={setCategory}/>
+        <FoodDisplay category={ category}/>
     </div>
   )
 }
