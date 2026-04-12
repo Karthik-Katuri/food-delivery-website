2143ac19f3de480a0dfb70a8d71ca7fa0d5da0be
unknown <kkaturi2003@gmail.com>
2026-04-12 18:54:23 +0530

    orders frontend done

---
+++ frontend/src/pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect, useContext } from 'react';
+import "./MyOrders.css";
+import { StoreContext } from '../../context/StoreContext';
+import axios from "axios";
+import { assets } from '../../assets/assets';
+
+const MyOrders = () => {
+    const { URL, token } = useContext(StoreContext);
+    const [data, setData] = useState([]);
+
+    const fetchOrders = async () => {
+        try {
+            const response = await axios.post(
+                URL + "/api/order/userorders",
+                {},
+                { headers: { token } }
+            );
+
+            setData(response.data.data);
+        } catch (error) {
+            console.error("Error fetching orders:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (token) {
+            fetchOrders();
+        }
+    }, [token]);
+
+    return (
+        <div className='my-orders'>
+            <h2>My Orders</h2>
+
+            <div className="container">
+                {data.length === 0 ? (
+                    <p>No orders found</p>
+                ) : (
+                    data.map((order, index) => (
+                        <div key={index} className="my-orders-order">
+                            <img src={assets.parcel_icon} alt="parcel" />
+
+                            <p>
+                               {
+                                order.items.map((item, index) => 
+                                   {
+                                     if(index===order.items.length-1){
+                                        return item.name+" x "+item.quantity
+                                    }else{
+                                        return item.name+" x "+item.quantity+", "
+                                    }
+                                   }
+                                )
+                               } 
+                            </p>
+
+                            <p>${order.amount}.00</p>
+                            <p>{order.status}</p>
+                        </div>
+                    ))
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default MyOrders;
